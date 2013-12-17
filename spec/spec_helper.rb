e566ba387591cbf1390c8117b02dc1162023c5c1
Wian Vos <wian.vos@kvk.nl>
2013-12-17 10:56:10 +0100

    corrected spec file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
+RSpec.configure do |c|
+  # Project root
+  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  # Readable test descriptions
+  c.formatter = :documentation
+
+end
+
